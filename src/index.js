6cf768d6bd498441023b9c18bd8e90570bf9ead7
amitdtu <amitdtu007@gmail.com>
2020-05-28 23:42:12 +0530

    edit api url 1.2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import "antd/dist/antd.css";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'antd/dist/antd.css';
+import axios from 'axios';
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/api/v1";
+axios.defaults.baseURL = 'https://still-cove-37639.herokuapp.com/api/v1';
 axios.defaults.withCredentials = true;
 axios.defaults.params = {
-  mediaURL: "http://127.0.0.1:8000",
+  mediaURL: 'http://127.0.0.1:8000',
 };
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
